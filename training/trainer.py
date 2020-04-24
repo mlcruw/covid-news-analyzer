50b15f4d027998c1e3bf1558a062938164265509
Udit Arora <uditara09@gmail.com>
2020-04-25 01:10:29 +0530

    Add initial Trainer code

---
+++ training/trainer.py
@@ -0,0 +1,114 @@
+from copy import copy
+import numpy as np
+import pandas as pd
+from sklearn.naive_bayes import GaussianNB, MultinomialNB
+from sklearn.svm import SVC
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import precision_recall_fscore_support
+
+model_class_map = {'gnb': GaussianNB, 'mnb': MultinomialNB,\
+    'svm': SVC, 'lr': LogisticRegression}
+
+class Trainer:
+    def __init__(self, data=None, models=None):
+        self.models = {}
+        if data is not None:
+            self.set_data(data)
+        if models is not None:
+            self.init_models(models)
+
+    def set_data(self, data):
+        """
+        Setup data arrays that will be used for training and validation
+
+        Args:
+        data: dictionary of numpy arrays containing training and validation data
+        """
+        keys = ['X_train', 'y_train', 'X_val', 'y_val']
+        for key in keys:
+            if key not in data:
+                raise Exception('Invalid data object passed to Trainer')
+        self.X_train = data['X_train']
+        self.y_train = data['y_train']
+        self.X_val = data['X_val']
+        self.y_val = data['y_val']
+    
+    def init_models(self, models):
+        """
+        Setup model objects that will be trained
+
+        Args:
+        models: list of models that will be trained on the data
+        """
+        for model in models:
+            self.add_model(model)
+    
+    def add_model(self, model):
+        """
+        Add (model, model_obj) to the models dictionary
+        """
+        if model not in self.models:
+            self.models[model] = model_class_map[model]()
+    
+    def train(self):
+        """
+        Train all the models on training data
+        """
+        for model_name, model_obj in self.models.items():
+            print("Training {}".format(model_name))
+            model_obj.fit(X_train, y_train)
+    
+    def train_model(self, model):
+        """
+        Train a specific model on training data
+        """
+        if model not in self.models:
+            raise Exception("{} doesn't exist in models".format(model))
+        self.models[model].fit(X_train, y_train)
+    
+    def evaluate(self):
+        """
+        Evaluate all the models on validation data and return metrics
+        stored in a pandas DataFrame
+        """
+        metrics = []
+        for model_name, model_obj in self.models.items():
+            y_pred = model_obj.predict(X_val)
+            precision, recall, f1, _ = precision_recall_fscore_support(y_val, y_pred, average='micro')
+            metrics.append({'precision': precision, 'recall': recall, 'f1-score': f1})
+        return pd.DataFrame(metrics, index=self.models.keys())
+
+if __name__=='__main__':
+    """
+    Unit test
+    """
+    n_train = 100
+    n_val = 20
+    n_feats = 20
+
+    X_train = np.random.rand(n_train, n_feats)
+    y_train = np.random.rand(n_train)
+    y_train[y_train>=0.5] = 1
+    y_train[y_train<0.5] = 0
+
+    X_val = np.random.rand(n_val, n_feats)
+    y_val = np.random.rand(n_val)
+    y_val[y_val>=0.5] = 1
+    y_val[y_val<0.5] = 0
+
+    data = {'X_train': X_train, 'y_train': y_train, 'X_val': X_val, 'y_val': y_val}
+
+    trainer = Trainer(data=data, models=['gnb', 'svm'])
+    # print(trainer.y_val)
+    # print(trainer.models)
+
+    trainer.train()
+    metrics = trainer.evaluate()
+    print(metrics)
+
+    print("Adding lr")
+
+    trainer.add_model('lr')
+    trainer.train_model('lr')
+    metrics = trainer.evaluate()
+    print(metrics)
