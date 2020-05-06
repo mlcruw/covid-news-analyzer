ee79fedadb7992a5af74a9d003cb0bdab00ea67c
strawberryfg <strawberryfgalois@gmail.com>
2020-05-06 02:44:15 -0400

    1. Update args 2. Uncomment config

--- training/argparser.py
+++ training/argparser.py
@@ -8,16 +8,17 @@ def argparser():
                       '--continue_train',
                       action='store_true',
                       help='when set, training will continue from before')
-
-  parser.add_argument('-t',
-                      '--test',
+  
+  #error: ambiguous option: --test could match --test_only, --test_ratio
+  parser.add_argument('-test_only',
+                      dest='test_only',
                       action='store_true',
-                      help='when set, testing will be done')
+                      help='when set, testing will be done (we are in the TEST PHASE, no need to train)')
 
   parser.add_argument('--dataset',
                       default='news_cat',
                       type=str,
-                      choices=['stan_sent', 'news_cat', 'fake_news'],
+                      choices=['stan_sent', 'news_cat', 'fake_news', 'emo_aff'],
                       help='dataset to use',
                       dest='dataset')
 
@@ -28,10 +29,11 @@ def argparser():
                       help='model(s) to run')
 
   # TODO: Add choices?
-  parser.add_argument('--feat',
+  parser.add_argument('--feats',
+                      nargs='+',
                       type=str,
                       help='features to use while training',
-                      dest='feat')
+                      dest='feats')
 
   parser.add_argument('--save_path',
                       type=str,
@@ -42,6 +44,19 @@ def argparser():
                       type=str,
                       help='provide path to where models were saved',
                       dest='load_path')
+
+  parser.add_argument('--split_ratio',
+                      type=float,
+                      default=0.9,
+                      help='train / val split ratio ($\in$ [0.0, 1.0])',
+                      dest='split_ratio')
+    
+  #optional: dataset is too large; takes too long to train or test the whole (This is for debugging convenience)
+  parser.add_argument('--test_ratio',
+                      type=float,
+                      default=0.0,
+                      help='ratio to test, when set to 0.0 uses the above val split ratio otherwise use test_ratio * num of samples',
+                      dest='test_ratio')
 
   args = parser.parse_args()
 
--- training/config.py
+++ training/config.py
@@ -4,17 +4,18 @@ import sys
 
 class Config:
   # Define some class attributes
-	## directory
-  # cur_dir = os.path.dirname(os.path.abspath(__file__))
-  # root_dir = os.path.join(cur_dir, '..')
+	## directory  
+  # Please do not comment these lines as they store some important directories that will be used later on in the trainer object. These are global configurations.  
+  cur_dir = os.path.dirname(os.path.abspath(__file__))
+  root_dir = os.path.join(cur_dir, '..')
   # datasets = ['stanford_sentiment']
 
   # change data directory
   # data_dir = os.path.join(root_dir, 'data/datasets')
-  # output_dir = os.path.join(root_dir, 'output')
-  # model_dir = os.path.join(output_dir, 'model_dump')
+  output_dir = os.path.join(root_dir, 'output')
+  model_dir = os.path.join(output_dir, 'model_dump')
   # vis_dir = os.path.join(output_dir, 'vis')
-  # log_dir = os.path.join(output_dir, 'log')
+  log_dir = os.path.join(output_dir, 'log')
   # result_dir = os.path.join(output_dir, 'result')
 
   ## model setting
@@ -26,31 +27,37 @@ class Config:
   ## testing config
 
   ## others
+  #Add some default arguments so that creating a new object Config() below and in trainer.py would not prompt any error message.
   def __init__(self,
-               dataset,
-               model,
-               feat,
-               save_path,
+               dataset='news_cat',
+               model='lr',
+               feats='bow',
+               save_path='bow_0',
                continue_train=False,
                load_path=None,
                test=None):
     self.dataset = dataset
     self.model = model
-    self.feat = feat
+    self.feats = feats
     self.save_path = save_path
     self.continue_train = continue_train
     self.load_path = load_path
-    self.testing = test
+    self.test = test
 
-# cfg = Config()
+#Do not comment "cfg" as it is a global configuration object that will be called later.
+cfg = Config()
 # sys.path.insert(0, cfg.root_dir)
 # sys.path.insert(0, os.path.join(cfg.root_dir, 'data'))
-# from utils.dir_utils import add_pypath, make_folder
+from utils.dir_utils import add_pypath, make_folder
 
 # add_pypath(os.path.join(cfg.data_dir))
 # for i in range(len(cfg.datasets)):
 #     add_pypath(os.path.join(cfg.data_dir, cfg.datasets[i]))
-# make_folder(cfg.model_dir)
+
+# Create the folder to store the best trained model
+make_folder(cfg.model_dir)
 # make_folder(cfg.vis_dir)
-# make_folder(cfg.log_dir)
+
+# Create the folder to store the log file
+make_folder(cfg.log_dir)
 # make_folder(cfg.result_dir)
