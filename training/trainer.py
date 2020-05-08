2e1ee930f47dee1749d7f9cc78384983673ed8a8
Udit Arora <uditara09@gmail.com>
2020-05-08 23:49:09 +0530

    Update trainer to use pipelines

--- training/trainer.py
+++ training/trainer.py
@@ -6,6 +6,8 @@ import pandas as pd
 from sklearn.naive_bayes import GaussianNB, MultinomialNB
 from sklearn.svm import SVC, LinearSVC
 from sklearn.linear_model import LogisticRegression
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+from sklearn.pipeline import Pipeline
 from sklearn.metrics import precision_recall_fscore_support
 #Do not comment as it contains the default configuration for trainer, see parameter for __init__ below
 from training.config import Config
@@ -15,12 +17,30 @@ import pickle
 from utils.logger import colorlogger
 
 
-model_class_map = {'gnb': GaussianNB, 'mnb': MultinomialNB,\
-    'svm': SVC, 'lr': LogisticRegression, 'linearsvm': LinearSVC}
+model_class_map = {
+    'gnb': GaussianNB, 'mnb': MultinomialNB,
+    'svm': SVC, 'lr': LogisticRegression, 'linearsvm': LinearSVC
+}
+
+transform_class_map = {
+    'bow': CountVectorizer,
+    'tfidf': TfidfVectorizer,
+    'ngram': CountVectorizer
+}
+
+transform_params = {
+    'bow': {'stop_words':'english', 'min_df':5},
+    'tfidf': {'stop_words':'english', 'min_df':5},
+    'ngram': {
+        'stop_words':'english', 'min_df':5,
+        'analyzer':'char_wb', 'ngram_range':(5, 5)
+    }
+}
 
 """
 #TODO:
 - Add capability to save best model found during training
+- Grid search for hyperparameters
 """
 class Trainer:
     """
@@ -36,11 +56,11 @@ class Trainer:
     #TODO: Pass dataset object here instead of data dict
     #TO-DO: the config and log_name as arguments?
     def __init__(self, dataset, models, transforms, cfg=Config(), log_name='logs.txt'):
-        self.models = {}
-        self.transformed = {}
+        self.pipelines = {}
+        self.models = models
+        self.transforms = transforms
         self.dataset = dataset
-        self.process_data(transforms)
-        self.init_models(models, transforms)
+        self.init_pipelines(models, transforms)
         # The current configuration for this trainer object
         self.cfg = cfg
         # The logger to saving (potentially) the config object and other stuff
@@ -51,6 +71,7 @@ class Trainer:
         Runs the feature transformations on the input data and create a dict
         containing features derived from all the transformations.
         """
+        self.transformed = {}
         for transform in transforms:
             self.transformed[transform] = {}
             X_train, X_val = FeatureExtractor(self.dataset.train_data['X'],
@@ -58,60 +79,77 @@ class Trainer:
             self.transformed[transform]['X_train'] = X_train
             self.transformed[transform]['X_val'] = X_val
 
-    def init_models(self, models, transforms):
+    def get_train_data(self):
         """
-        Setup model objects that will be trained for each model x transform
+        Returns X_train, y_train
+        """
+        return self.dataset.train_data['X'].values, self.dataset.train_data['y'].values
+
+    def get_test_data(self):
+        """
+        Returns X_test, y_test
+        """
+        return self.dataset.train_data['X'], self.dataset.train_data['y']
+
+    def init_pipelines(self, models, transforms):
+        """
+        Setup pipeline objects that will be trained for each model x transform
 
         Args:
-        models: list of models that will be trained on the data
+        models (list): list of models that will be trained on the data
+        transforms (list): list of feature transformations to be applied on the data
         """
         for model in models:
-            self.add_model(model, transforms)
-
-    def add_model(self, model, transforms):
-        """
-        Add (model, model_obj) for each transform to the models dictionary
-        """
-        if model not in self.models:
-            self.models[model] = {}
+            if model not in self.pipelines:
+                self.pipelines[model] = {}
             for transform in transforms:
-                self.models[model][transform] = model_class_map[model]()
+                self.add_pipeline(model, transform)
+
+    def add_pipeline(self, model, transform):
+        """
+        Add a pipeline object with given transform and model
+        """
+        tranform_params = transform_params[transform]
+        transform_obj = transform_class_map[transform](**tranform_params)
+        model_obj = model_class_map[model]()
+        steps = [('tranform', transform_obj),('model', model_obj)]
+        self.pipelines[model][transform] = Pipeline(steps, verbose=True)
 
     def train(self):
         """
         Train all the models on training data
         """
-        for model in self.models:
-            for transform in self.models[model]:
-                model_obj = self.models[model][transform]
-                X_train = self.transformed[transform]['X_train']
+        for model in self.pipelines:
+            for transform in self.pipelines[model]:
+                pipeline_obj = self.pipelines[model][transform]
+                X_train, y_train = self.get_train_data()
                 print("Training {} with {} transformation".format(model, transform))
                 self.logger.info("Training {} with {} transformation".format(model, transform))
-                model_obj.fit(X_train, self.dataset.train_data['y'])
+                pipeline_obj.fit(X_train, y_train)
 
     def train_model(self, model):
         """
         Train a specific model on all transformations of training data
         """
-        if model not in self.models:
+        if model not in self.pipelines:
             raise Exception("{} doesn't exist in models".format(model))
-        for transform in self.models[model]:
-            X_train = self.transformed[transform]['X_train']
-            self.models[model][transform].fit(X_train, self.dataset.train_data['y'])
+        for transform in self.pipelines[model]:
+            X_train, y_train = self.get_train_data()
+            self.pipelines[model][transform].fit(X_train, y_train)
 
     def save_model(self, model, transform, model_path):
         """
         Save a specific model to the path : "model_path"
         """
-        if model not in self.models:
-            raise Exception("{} doesn't exist in models".format(model))
-        if transform not in self.models[model]:
-            raise Exception("{} transformation doesn't exist in models".format(transform))
+        if model not in self.pipelines:
+            raise Exception("{} doesn't exist in pipelines".format(model))
+        if transform not in self.pipelines[model]:
+            raise Exception("{} transformation doesn't exist in pipelines".format(transform))
         #It uses the configuration to find the model directory
         #Config should be imported somehow so that it knows where the folder is
         file_path = os.path.join(self.cfg.model_dir,model_path)
         with open(file_path, 'wb') as fw:
-            pickle.dump(self.models[model][transform], fw)
+            pickle.dump(self.pipelines[model][transform], fw)
 
         # Output the associated config object to log
         self.logger.info("Saving configuration:")
@@ -122,13 +160,13 @@ class Trainer:
         """
         Load a specific model from the path : "model_path"
         """
-        if model not in self.models:
+        if model not in self.pipelines:
             raise Exception("{} doesn't exist in models".format(model))
         #It uses the configuration to find the model directory
         #Config should be somewhere called to see the folder path
         file_path = os.path.join(self.cfg.model_dir,model_path)
         with open(file_path, 'rb') as file:
-            self.models[model] = pickle.load(file)
+            self.pipelines[model] = pickle.load(file)
 
         # Tell what the loading configuration looks like
         self.logger.info("Loading from this configuration:")
@@ -140,13 +178,13 @@ class Trainer:
         stored in a pandas DataFrame
         """
         metrics = []
-        for model in self.models:
-            for transform in self.models[model]:
-                model_obj = self.models[model][transform]
-                X_val = self.transformed[transform]['X_val']
-                y_pred = model_obj.predict(X_val)
+        for model in self.pipelines:
+            for transform in self.pipelines[model]:
+                pipeline_obj = self.pipelines[model][transform]
+                X_test, y_test = self.get_test_data()
+                y_pred = pipeline_obj.predict(X_test)
                 precision, recall, f1, _ = precision_recall_fscore_support(
-                    self.dataset.test_data['y'], y_pred, average='micro')
+                    y_test, y_pred, average='micro')
                 metric_dict = {
                     'model': model,
                     'transform': transform,
@@ -161,33 +199,34 @@ if __name__=='__main__':
     """
     Unit test
     """
-    n_train = 100
-    n_val = 20
-    n_feats = 20
+    pass
+    # n_train = 100
+    # n_val = 20
+    # n_feats = 20
 
-    X_train = np.random.rand(n_train, n_feats)
-    y_train = np.random.rand(n_train)
-    y_train[y_train>=0.5] = 1
-    y_train[y_train<0.5] = 0
+    # X_train = np.random.rand(n_train, n_feats)
+    # y_train = np.random.rand(n_train)
+    # y_train[y_train>=0.5] = 1
+    # y_train[y_train<0.5] = 0
 
 
-    X_val = np.random.rand(n_val, n_feats)
-    y_val = np.random.rand(n_val)
-    y_val[y_val>=0.5] = 1
-    y_val[y_val<0.5] = 0
+    # X_val = np.random.rand(n_val, n_feats)
+    # y_val = np.random.rand(n_val)
+    # y_val[y_val>=0.5] = 1
+    # y_val[y_val<0.5] = 0
 
-    data = {'X_train': X_train, 'y_train': y_train, 'X_val': X_val, 'y_val': y_val}
+    # data = {'X_train': X_train, 'y_train': y_train, 'X_val': X_val, 'y_val': y_val}
 
-    trainer = Trainer(data=data, models=['gnb', 'svm'], transforms=['bow'])
-    # print(trainer.y_val)
-    # print(trainer.models)
+    # trainer = Trainer(data=data, models=['gnb', 'svm'], transforms=['bow'])
+    # # print(trainer.y_val)
+    # # print(trainer.models)
 
-    trainer.train()
-    metrics = trainer.evaluate()
-    print(metrics)
+    # trainer.train()
+    # metrics = trainer.evaluate()
+    # print(metrics)
 
-    print("Adding lr")
-    trainer.add_model('lr', ['bow'])
-    trainer.train_model('lr')
-    metrics = trainer.evaluate()
-    print(metrics)
+    # print("Adding lr")
+    # trainer.add_model('lr', ['bow'])
+    # trainer.train_model('lr')
+    # metrics = trainer.evaluate()
+    # print(metrics)
